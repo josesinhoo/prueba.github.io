b97021e0d4e750821b59454f8506210fc66ec060
josesinhoo <129109647+josesinhoo@users.noreply.github.com>
2023-04-24 15:10:39 -0500

    main

---
+++ js/operador.js
@@ -0,0 +1,8 @@
+//CONVERSION LARGA:
+var celsius =prompt("Cuantos grados celsius son?  ");
+celsius = parseFloat(celsius)
+
+//CONVERSION CORTA
+var celsius =parseFloat(prompt("Introduzca la temperatutra en grados celsius: "))
+var f = 9/5*celsius+32
+document.write("La conversion es: ". f )
